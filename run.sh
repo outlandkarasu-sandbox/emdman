7f9fa030af20eb8d4026129ef64f88ab9d48166e
outlandkarasu <outland.karasu@gmail.com>
2018-12-05 22:38:33 +0900

    SDL2を使用したビルドが行えるまで実装した。

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+docker-compose run --rm dlang /bin/bash
+
--- source/app.d
+++ source/app.d
@@ -1,6 +1,25 @@
-import std.stdio;
+import bindbc.sdl;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+alias em_callback_func = extern(C) void function() @nogc nothrow;
+extern(C) void emscripten_set_main_loop(em_callback_func func, int fps, int simulate_infinite_loop) @nogc nothrow;
+
+extern(C) int main(int argc, const char** argv) @nogc nothrow {
+    SDL_Init(SDL_INIT_VIDEO);
+    scope(exit) SDL_Quit();
+
+    auto window = SDL_CreateWindow(
+        "SDL Tutorial",
+	SDL_WINDOWPOS_UNDEFINED,
+	SDL_WINDOWPOS_UNDEFINED,
+	640,
+	480,
+	SDL_WINDOW_SHOWN);
+    scope(exit) SDL_DestroyWindow(window);
+
+    emscripten_set_main_loop(&mainLoop, 60, 1);
+    return 0;
 }
+
+extern(C) void mainLoop() @nogc nothrow {
+}
+
